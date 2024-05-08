fa799ea353fa2ded4db01f583f1d6745eddb3b1f
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-05-08 17:46:05 +0530

    validator uitl added

--- src/utils/helpers/validator.ts
+++ src/utils/helpers/validator.ts
@@ -4,48 +4,46 @@ import { BadRequestError } from '../core/ApiError';
 import { Types } from 'mongoose';
 
 export enum ValidationSource {
-  BODY = 'body',
-  HEADER = 'headers',
-  QUERY = 'query',
-  PARAM = 'params',
+    BODY = 'body',
+    HEADER = 'headers',
+    QUERY = 'query',
+    PARAM = 'params',
 }
 
 export const JoiObjectId = () =>
-  Joi.string().custom((value: string, helpers) => {
-    if (!Types.ObjectId.isValid(value)) return helpers.error('any.invalid');
-    return value;
-  }, 'Object Id Validation');
+    Joi.string().custom((value: string, helpers) => {
+        if (!Types.ObjectId.isValid(value)) return helpers.error('any.invalid');
+        return value;
+    }, 'Object Id Validation');
 
-export const JoiUrlEndpoint = () =>
-  Joi.string().custom((value: string, helpers) => {
+export const JoiUrlEndpoint = () =>Joi.string().custom((value: string, helpers) => {
     if (value.includes('://')) return helpers.error('any.invalid');
     return value;
-  }, 'Url Endpoint Validation');
+}, 'Url Endpoint Validation');
 
 export const JoiAuthBearer = () =>
-  Joi.string().custom((value: string, helpers) => {
-    if (!value.startsWith('Bearer ')) return helpers.error('any.invalid');
-    if (!value.split(' ')[1]) return helpers.error('any.invalid');
-    return value;
-  }, 'Authorization Header Validation');
+    Joi.string().custom((value: string, helpers) => {
+        if (!value.startsWith('Bearer ')) return helpers.error('any.invalid');
+        if (!value.split(' ')[1]) return helpers.error('any.invalid');
+        return value;
+}, 'Authorization Header Validation');
 
 export default (
     schema: Joi.AnySchema,
     source: ValidationSource = ValidationSource.BODY,
-  ) =>
-  (req: Request, res: Response, next: NextFunction) => {
+) =>
+    (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { error } = schema.validate(req[source]);
+        const { error } = schema.validate(req[source]);
+        if (!error) return next();
 
-      if (!error) return next();
-
-      const { details } = error;
-      const message = details
+        const { details } = error;
+        const message = details
         .map((i) => i.message.replace(/['"]+/g, ''))
         .join(',');
 
-      next(new BadRequestError(message));
+        next(new BadRequestError(message));
     } catch (error) {
-      next(error);
+        next(error);
     }
-  };
+};
